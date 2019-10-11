9097e624a51cbf6decfa55368d882d95bbd63500
Nathan Blau <nate_blau@yahoo.com>
2019-10-11 01:25:25 -0700

    Added Asteroid Class

---
+++ Asteroids/Asteroid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Asteroids
+{
+	class Asteroid : GameObject
+	{
+		private int rotationDir;
+		private double rotationSpeed;
+
+
+		public Asteroid(int x, int y, int r, Vector v, int RotationDir, double RotationSpeed) : base(x, y, r, v)
+		{
+			Angle = 0;
+
+			rotationDir = RotationDir;
+			rotationSpeed = RotationSpeed;
+
+			base.points = new Point[8];
+			UpdatePoints();
+		}
+
+
+
+
+
+
+		private void UpdatePoints()
+		{
+
+
+			for (int i = 0; i < points.Length; i++)
+			{
+				points[i].X = this.XPos + (int)(this.HitBoxRadius * Math.Cos(Angle + (i * Math.PI) / ((points.Length /2) - 1)));
+				points[i].Y = this.YPos + (int)(this.HitBoxRadius * Math.Sin(Angle + (i * Math.PI) / ((points.Length /2) - 1)));
+			}
+
+		}
+
+
+		public override void UpdatePostion()
+		{
+			base.UpdatePostion();
+			Angle += rotationSpeed * rotationDir;
+			UpdatePoints();
+		}
+
+	}
+}
